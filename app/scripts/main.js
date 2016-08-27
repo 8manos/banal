996d929e75bfd15cad4c44ddb83bd968c56fa04c
Anabelle Handdoek <ana@gathacol.net>
2016-08-27 09:22:41 -0500

    funcion para mostrar y ocultar spans anidados

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+	$('a.mostrar_span').on( 'click', function( e ){
+		e.preventDefault();
+		if( $(this).parents('span').hasClass('open') ){
+			$(this).parents('span').removeClass('open');
+		}else{
+			$(this).parents('span').addClass('open');
+		}
+	});
+});
